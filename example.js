9aa6f0963ada2ad738601f7589e8d5b6032a5153
Nikolay Botev <nikolaybotev.b@gmail.com>
2024-09-12 16:32:53 +0300

    Emit data on prototypes

--- example.js
+++ example.js
@@ -20,3 +20,21 @@ const gen = asyncGenerator();
 console.log(Object.getPrototypeOf(gen)); // function asyncGenerator()
 console.log(Object.getPrototypeOf(Object.getPrototypeOf(gen))); // AsyncGenerator
 console.log(Object.getPrototypeOf(Object.getPrototypeOf(Object.getPrototypeOf(gen)))); // AsyncIterator
+
+const myGenerator = function* (){}();
+const myAsyncGenerator = async function* (){}();
+console.log("drop() on array.values(): ", [].values().drop);
+console.log("drop() on generator", myGenerator.drop);
+console.log("drop() on async generator", myAsyncGenerator.drop);
+
+console.log("generator proto", myGenerator.__proto__);
+console.log("generator proto.proto", myGenerator.__proto__.__proto__);
+console.log("generator proto.proto.proto", myGenerator.__proto__.__proto__.__proto__);
+console.log("generator proto.proto.proto.proto", myGenerator.__proto__.__proto__.__proto__.__proto__);
+console.log("generator proto.proto.proto.proto", myGenerator.__proto__.__proto__.__proto__.__proto__.__proto__);
+console.log("array.values() proto", [].values().__proto__);
+console.log("array.values() proto.proto", [].values().__proto__.__proto__);
+console.log("array.values() proto.proto.proto", [].values().__proto__.__proto__.__proto__);
+console.log("array.values() proto.proto.proto.proto", [].values().__proto__.__proto__.__proto__.__proto__);
+// console.log("generator proto", myGenerator.__proto__);
+// console.log("generator proto", myGenerator.__proto__);
