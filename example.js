3314816ad795a39ad25f2ad79960b61da76cd874
Nikolay Botev <nikolaybotev.b@gmail.com>
2024-09-12 16:02:01 +0300

    Some tests

--- example.js
+++ example.js
@@ -4,3 +4,16 @@ async function* asyncGenerator() {
   yield 3;
 }
 
+async function main() {
+  for await (const x of asyncGenerator()) {
+    console.log(x);
+  }
+}
+
+main().then(() => console.log("done"));
+
+const gen = asyncGenerator();
+
+console.log(Object.getPrototypeOf(gen)); // function asyncGenerator()
+console.log(Object.getPrototypeOf(Object.getPrototypeOf(gen))); // AsyncGenerator
+console.log(Object.getPrototypeOf(Object.getPrototypeOf(Object.getPrototypeOf(gen)))); // AsyncIterator
