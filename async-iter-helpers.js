60c1782dcd5a653a4ea06883bc3aa9397611414d
Nikolay Botev <nikolaybotev.b@gmail.com>
2024-09-15 17:04:51 +0300

    add Async Iterator helpers test

---
+++ async-iter-helpers.js
@@ -0,0 +1,27 @@
+//import configurator from "core-js/configurator.js";
+{
+  const asyncGeneratorInstancePrototype = Object.getPrototypeOf(async function*(){}());
+  const AsyncGeneratorPrototype = Object.getPrototypeOf(asyncGeneratorInstancePrototype);
+  let AsyncIteratorPrototype;
+  if (AsyncGeneratorPrototype === Object.prototype) {
+    // Fix-up for babel's transform-async-generator-functions
+    console.log("FIXUP");
+    AsyncIteratorPrototype = {};
+    Object.setPrototypeOf(asyncGeneratorInstancePrototype, AsyncIteratorPrototype);
+  } else {
+    AsyncIteratorPrototype = Object.getPrototypeOf(AsyncGeneratorPrototype);
+  }
+  (await import("core-js/configurator.js")).default({ AsyncIteratorPrototype });
+}
+await import("core-js/proposals/async-iterator-helpers.js");
+
+async function* gen() {
+  yield* [1, 2, 3, 4, 5];
+}
+
+gen()
+  .drop(1)
+  .take(3)
+  .filter(n => n != 3)
+  .map(n => n * 10)
+  .forEach(n => console.log(n));
